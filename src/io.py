9b07d662cf432c197620a8a268cfbd3412ab3b18
aliabbasi7898 <aliabbasi7898@gmail.com>
2022-01-30 18:42:39 +0330

    normilizing

--- src/io.py
+++ src/io.py
@@ -40,3 +40,14 @@ def read_tsp(filename):
         # cities.set_index('city', inplace=True)
 
         return cities
+def normalize(points):
+    """
+    Return the normalized version of a given vector of points.
+    For a given array of n-dimensions, normalize each dimension by removing the
+    initial offset and normalizing the points in a proportional interval: [0,1]
+    on y, maintining the original ratio on x.
+    """
+    ratio = (points.x.max() - points.x.min()) / (points.y.max() - points.y.min()), 1
+    ratio = np.array(ratio) / max(ratio)
+    norm = points.apply(lambda c: (c - c.min()) / (c.max() - c.min()))
+    return norm.apply(lambda p: ratio * p, axis=1)
