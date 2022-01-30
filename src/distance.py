5cd3ae9ccfffa42a35f7cf586d227f78d98dcb64
aliabbasi7898 <aliabbasi7898@gmail.com>
2022-01-30 17:53:09 +0330

    distance init

---
+++ src/distance.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from distance import select_closest
+
+def generate_network(size):
+    """
+    Generate a neuron network of a given size.
+    Return a vector of two dimensional points in the interval [0,1].
+    """
+    return np.random.rand(size, 2)
