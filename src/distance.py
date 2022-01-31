87956585100c8a514f2cbcf706286d8c01fb0145
aliabbasi7898 <aliabbasi7898@gmail.com>
2022-01-31 20:13:56 +0330

    distance corrected

--- src/distance.py
+++ src/distance.py
@@ -1,30 +1,15 @@
 import numpy as np
 
-from distance import select_closest
+def select_closest(candidates, origin):
+    """Return the index of the closest candidate to a given point."""
+    return euclidean_distance(candidates, origin).argmin()
 
-def generate_network(size):
-    """
-    Generate a neuron network of a given size.
-    Return a vector of two dimensional points in the interval [0,1].
-    """
-    return np.random.rand(size, 2)
-def get_neighborhood(center, radix, domain):
-    """Get the range gaussian of given radix around a center index."""
+def euclidean_distance(a, b):
+    """Return the array of distances of two numpy arrays of points."""
+    return np.linalg.norm(a - b, axis=1)
 
-    # Impose an upper bound on the radix to prevent NaN and blocks
-    if radix < 1:
-        radix = 1
-
-    # Compute the circular network distance to the center
-    deltas = np.absolute(center - np.arange(domain))
-    distances = np.minimum(deltas, domain - deltas)
-
-    # Compute Gaussian distribution around the given center
-    return np.exp(-(distances*distances) / (2*(radix*radix)))
-def get_route(cities, network):
-    """Return the route computed by a network."""
-    cities['winner'] = cities[['x', 'y']].apply(
-        lambda c: select_closest(network, c),
-        axis=1, raw=True)
-
-    return cities.sort_values('winner').index
+def route_distance(cities):
+    """Return the cost of traversing a route of cities in a certain order."""
+    points = cities[['x', 'y']]
+    distances = euclidean_distance(points, np.roll(points, 1, axis=0))
+    return np.sum(distances)
